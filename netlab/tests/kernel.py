c063d0314a4a872733f4fa13d3fb9d60790c2fd1
Matous Holinka <matous.holinka@nic.cz>
2020-03-20 18:50:26 +0100

    dev: tests/base.py + tests/kernel.py were refactored

--- netlab/tests/kernel.py
+++ netlab/tests/kernel.py
@@ -1,62 +1,68 @@
 import os
 import sys
+import pickle
 import subprocess
 from time import sleep
-from inspect import getsourcefile
-
-current_dir = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
-sys.path.insert(0, current_dir[: current_dir.rfind(os.path.sep)])
-
-import tests.config as cf
 
 
-def wait(sec):
+def test_krt_routes(dev, protocol):
+    tdir, mod = load_args_from_file()
+    if mod == "save":
+        save_krt_routes(dev, tdir)
+    else:
+        check_krt_routes_timeout(dev, tdir)
+        check_protocol_state(dev, protocol)
+
+
+def wait(sec: int) -> None:
     sleep(sec)
 
 
-def save_krt_routes(key, dev, ip=4):
-    """Get the tables and process them as referencial data"""
-    filename = f"{cf.datadir}/{key}-{dev}"
+def load_args_from_file():
+    with open("common/runtest_args.pckl", "rb") as args_file:
+        return pickle.load(args_file)
+
+
+def modify_command(dev: str) -> str:
+    return f"cd {dev} && sudo ./birdc -l show protocols"
+
+
+def get_attributes_from_stdout(data: str, protocol: str) -> list:
+    return [line.split() for line in data.split("\n") if line.startswith(protocol)]
+
+
+def check_running_ospf(data: list) -> None:
+    assert "Running" in data
+
+
+def check_established_bgp(data: list) -> None:
+    assert "Established" in data
+
+
+def save_stdout(cmd: str) -> str:
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    proc_stdout = process.communicate()[0].strip()
+    return proc_stdout.decode("utf-8")
+
+
+def read_write_routes(name: str, mode: str, content: str = None) -> None:
+    with open(name, mode) as txt:
+        if mode == "w":
+            txt.write(content)
+        elif mode == "r":
+            return txt.read()
+
+
+def save_krt_routes(dev: str, procotol: str, ip: int = 4) -> None:
+    """Get the tables, process tables and save them"""
+    filename = f"{procotol}/data/krt-{dev}"
     table_content = save_stdout(f"sudo ip netns exec {dev} ip -{ip} route show")
-    write_krt_routes(filename, table_content)
+    read_write_routes(filename, mode="w", content=table_content)
 
 
-def write_krt_routes(name, content):
-    with open(name, "w") as txt:
-        txt.write(content)
-
-
-def read_krt_routes(name):
-    with open(name, "r") as txt:
-        return txt.read().split("\n")
-
-
-def check_krt_routes(key, dev, ip=4):
-    """Check the content of actual tables and the original"""
-    filename = f"{cf.datadir}/{key}-{dev}"
-    mn_table_cont = save_stdout(f"ip netns exec {dev} ip -{ip} route show").split("\n")
-    content = read_krt_routes(filename)
-
-    for _ in mn_table_cont:
-        return mn_table_cont == content
-
-
-# TIME MEASURE
-# def check_krt_routes(key, dev, ip=4):
-#     """Check the content of actual tables and the original"""
-#     filename = f"cases/cf-ospf-base/data/{key}-{dev}"
-#     mn_table_cont = save_stdout(f"sudo ip netns exec {dev} ip -{ip} route show").split(
-#         "\n"
-#     )
-#     content = read_krt_routes(filename)
-
-#     for _ in mn_table_cont:
-#         return mn_table_cont == content
-
-
-def check_krt_routes_timeout(key, dev, timeout=60):
+def check_krt_routes_timeout(dev, tdir, timeout=60):
     for sec in range(timeout):
-        if check_krt_routes(key, dev):
+        if check_krt_routes(dev, tdir):
             assert True
         elif sec == timeout - 1:
             assert False
@@ -64,20 +70,14 @@ def check_krt_routes_timeout(key, dev, timeout=60):
             sleep(1)
 
 
-def modify_command(dev: str) -> str:
-    return f"cd {dev} && sudo ./birdc -l show protocols"
+def check_krt_routes(dev: str, protocol: str, ip: int = 4) -> bool:
+    """Check the content of actual tables and the original"""
+    filename = f"{protocol}/data/krt-{dev}"
+    mn_table_cont = save_stdout(f"ip netns exec {dev} ip -{ip} route show").split("\n")
+    content = read_write_routes(filename, mode="r").split("\n")
 
-
-def save_stdout(cmd: str) -> str:
-    """Run the command :cmd: and return it as variable (stdout)"""
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    proc_stdout = process.communicate()[0].strip()
-    return proc_stdout.decode("utf-8")
-
-
-def get_attributes_from_output(data: str, protocol: str) -> list:
-    """Parse the string from parameter and return list"""
-    return [line.split() for line in data.split("\n") if line.startswith(protocol)]
+    for _ in mn_table_cont:
+        return mn_table_cont == content
 
 
 def check_protocol_state(dev: str, protocol: str) -> None:
@@ -90,7 +90,7 @@ def check_protocol_state(dev: str, protocol: str) -> None:
     """
     command = modify_command(dev)
     decoded = save_stdout(command)
-    clean_str = get_attributes_from_output(decoded, protocol)
+    clean_str = get_attributes_from_stdout(decoded, protocol)
 
     for line in clean_str:
         if protocol == "ospf":
@@ -109,21 +109,5 @@ def check_nodes_with_password(dev: str, protocol: str) -> dict:
     """
     command = modify_command(dev)
     decoded = save_stdout(command)
-    cleaned = get_attributes_from_output(decoded, protocol)
+    cleaned = get_attributes_from_stdout(decoded, protocol)
     return {str(index): {lst[0]: lst[-1]} for index, lst in enumerate(cleaned)}
-
-
-def check_running_ospf(data: list) -> None:
-    assert "Running" in data
-
-
-def check_established_bgp(data: list) -> None:
-    assert "Established" in data
-
-
-def test_krt_routes(key, dev, protocol):
-    if cf.save:
-        save_krt_routes(key, dev)
-    else:
-        check_krt_routes_timeout(key, dev)
-        check_protocol_state(dev, protocol)
