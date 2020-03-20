a003c290d6a0cdd2de945e480da8af4815eee622
Matous Holinka <matous.holinka@nic.cz>
2020-03-20 18:49:56 +0100

    dev: test suits were modified

--- netlab/cases/cf-ospf/test-cf-ospf.py
+++ netlab/cases/cf-ospf/test-cf-ospf.py
@@ -1,16 +1,29 @@
+import os
+import sys
+import pickle
 import pytest
+from inspect import getsourcefile
+
+current_dir = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+sys.path.insert(0, current_dir.rsplit(os.path.sep, 2)[0])
 import tests.kernel as tk
 
-LIMIT = 60
+sys.path.pop(0)
+
+
+_LIMIT = 60
+
+with open("common/runtest_args.pckl", "rb") as args_file:
+    testdir, mode = pickle.load(args_file)
 
 
 @pytest.mark.skipif(
-    tk.cf.save == False,
+    mode == "check",
     reason="No need to verify time of convergence during the check mode",
 )
 def test_wait():
     """Wait until the time (limit) runs out"""
-    tk.wait(LIMIT)
+    tk.wait(_LIMIT)
 
 
 @pytest.mark.parametrize(
@@ -22,4 +35,4 @@ def test_krt_routes(expected_device,):
     -------------------
          - default parametr :limit: set to the value 60 seconds
     """
-    tk.test_krt_routes("krt", expected_device, "ospf")
+    tk.test_krt_routes(expected_device, "ospf")
