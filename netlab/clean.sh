9d16d3bb7a5286b2d5b3b1b0363b764f61d0e5ff
Matous Holinka <matous.holinka@nic.cz>
2020-03-25 13:40:44 +0100

    dev: netlab/clean.sh modified

--- netlab/clean.sh
+++ netlab/clean.sh
@@ -2,8 +2,8 @@
 
 # cleaning script 
 # if there is running device inside the root folder
-if [ -L "config" ];
-then ./stop
+if [ -L "config" ]; then
+    ./stop
 fi
 
 # this part is automatic
@@ -12,5 +12,9 @@ find . -type f -name "*.py[co]" -delete -or -type d -name "__pycache__" -delete
 
 if [ -d "temp" ]; then
     rm -r temp/
-    rm common/runtest_args.pckl
+fi
+
+# delete runtest's arguments if there is such file 
+if [ -f "common/runtest_args.pckl" ]; then
+	rm common/runtest_args.pckl
 fi
