9341780f62017f978d785efe25781d8b10f91bc4
Matous Holinka <matous.holinka@nic.cz>
2020-03-26 17:02:55 +0100

    mh-bgp-base-tests: basic test suite was modified (due to bgp sessions issues)

--- netlab/cases/cf-bgp-base/test-cf-bgp-base.py
+++ netlab/cases/cf-bgp-base/test-cf-bgp-base.py
@@ -10,44 +10,54 @@ import tests.kernel as tk
 
 sys.path.pop(0)
 
-
-_LIMIT = 60
-
 with open("common/runtest_args.pckl", "rb") as args_file:
     testdir, mode = pickle.load(args_file)
+
+LIMIT = 60
+EXPECTED_DEVICES = (
+    "m11",
+    "m12",
+    "m13",
+    "m14",
+    "m21",
+    "m22",
+    "m23",
+    "m24",
+    "m31",
+    "m32",
+    "m33",
+    "m34",
+    "m41",
+    "m42",
+    "m43",
+    "m44",
+)
 
 
 @pytest.mark.skipif(mode == "check", reason="mode: save")
 def test_wait():
     """Wait until the time (limit) runs out"""
-    tk.wait(_LIMIT)
+    tk.wait(LIMIT)
 
 
-@pytest.mark.parametrize(
-    "expected_device",
-    [
-        "m11",
-        "m12",
-        "m13",
-        "m14",
-        "m21",
-        "m22",
-        "m23",
-        "m24",
-        "m31",
-        "m32",
-        "m33",
-        "m34",
-        "m41",
-        "m42",
-        "m43",
-        "m44",
-    ],
-)
-def test_krt_routes(expected_device):
-    """
-    # Basic test case:
-    1. Check the krt tables in a while
-    2. Check the status of specific protocols
-    """
-    tk.test_krt_routes(expected_device, "bgp")
+@pytest.mark.skipif(mode == "check", reason="mode: save")
+@pytest.mark.parametrize("exp_devs", EXPECTED_DEVICES)
+def test_save_krt_routes(exp_devs):
+    tk.save_krt_routes(exp_devs, testdir)
+
+
+@pytest.mark.skipif(mode == "save", reason="mode: check")
+@pytest.mark.parametrize("exp_devs", EXPECTED_DEVICES)
+def test_check_krt_routes_timeout(exp_devs):
+    tk.check_krt_routes_timeout(exp_devs, testdir)
+
+
+@pytest.mark.skipif(mode == "save", reason="mode: check")
+def test_wait_bgp():
+    tk.wait(10)
+
+
+@pytest.mark.skipif(mode == "save", reason="mode: check")
+@pytest.mark.parametrize("exp_devs", EXPECTED_DEVICES)
+def test_check_protocol_state(exp_devs):
+    tk.check_protocol_state(exp_devs, "bgp")
