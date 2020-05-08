36c63ae01466efbe85aa12de0f68f45e79924295
Matous Holinka <matous.holinka@nic.cz>
2020-05-08 21:26:51 +0200

    mh-major-fixes: test-cases now have no folder

---
+++ netlab/cf-ospf-default/test-cf-ospf-default.py
@@ -0,0 +1,25 @@
+import pytest
+import tests.kernel as tk
+
+LIMIT = 60
+
+
+@pytest.mark.skipif(
+    tk.cf.save == False,
+    reason="No need to verify time of convergence during the check mode",
+)
+def test_wait():
+    """Wait until the time (limit) runs out"""
+    tk.wait(LIMIT)
+
+
+@pytest.mark.parametrize(
+    "expected_device", ["m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8"],
+)
+def test_krt_routes(expected_device):
+    """
+    Test kernel routes:
+    -------------------
+         - default parametr :limit: set to the value 60 seconds
+    """
+    tk.test_krt_routes("krt", expected_device)
